d9b82054ff37e40004a55f2061f767a4891cf4c5
Elijah Oyekunle <eloyekunle@gmail.com>
2018-06-17 20:43:52 +0100

    initial commit

---
+++ PermissionsBundle.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright (c) 2018. Elijah Oyekunle <eloyekunle@gmail.com>.
+ * Please view the LICENSE file for the full copyright and license information.
+ */
+
+namespace Eloyekunle\PermissionsBundle;
+
+
+class PermissionsBundle
+{
+
+}
