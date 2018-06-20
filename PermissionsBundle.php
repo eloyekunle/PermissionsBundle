1b98c5a01386f515c4701767350d6211a9b57f98
Elijah Oyekunle <eloyekunle@gmail.com>
2018-06-20 15:42:17 +0100

    removed license from code files

--- PermissionsBundle.php
+++ PermissionsBundle.php
@@ -1,8 +1,4 @@
 <?php
-/**
- * Copyright (c) 2018. Elijah Oyekunle <eloyekunle@gmail.com>.
- * Please view the LICENSE file for the full copyright and license information.
- */
 
 namespace Eloyekunle\PermissionsBundle;
 
