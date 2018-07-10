5b22cee7a7d3b6124ec98b3d1a5f10fddca0a84b
Elijah Oyekunle <eloyekunle@gmail.com>
2018-07-10 14:13:25 +0100

    added test stubs

---
+++ Tests/Util/YamlDiscoveryTest.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the EloyekunlePermissionsBundle package.
+ *
+ * (c) Elijah Oyekunle <https://elijahoyekunle.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eloyekunle\PermissionsBundle\Tests\Util;
+
+use PHPUnit\Framework\TestCase;
+
+class YamlDiscoveryTest extends TestCase
+{
+}
