9df4feb6db78eade2d125e4ce5dcae03686777ee
Elijah Oyekunle <eloyekunle@gmail.com>
2019-02-17 17:06:25 +0100

    update treebuilder

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -22,8 +22,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('eloyekunle_permissions');
+        $treeBuilder = new TreeBuilder('eloyekunle_permissions');
+        $rootNode = method_exists(TreeBuilder::class, 'getRootNode') ? $treeBuilder->getRootNode() : $treeBuilder->root('eloyekunle_permissions');
 
         $supportedDrivers = ['orm'];
 
